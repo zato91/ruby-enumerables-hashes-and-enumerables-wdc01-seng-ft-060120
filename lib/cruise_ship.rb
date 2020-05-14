df592b9d2454f85429d772b5620905c3b314c694
zara ahmadi <zatokey@gmail.com>
2020-05-14 17:47:15 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,12 @@
 # }
 
 def select_winner(passengers)
-  passengers.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
   end
+end
+ 
+winner
 end
